56892d336469157196ec73e227ac144a6411933a
Arber <arber@Arbers-MBP.fritz.box>
2023-07-31 15:10:13 +0200

    created some files in client/still have to write functions on components and on api

---
+++ client/src/context/Container.jsx
@@ -0,0 +1,24 @@
+import {MyContext} from "./Context"
+import Reducer from "../reducers/Reducer.js"
+import {useEffect,useReducer,useState} from "react"
+import * as api from "../api/index.js "
+
+const initialState={posts:[],user:null} 
+const Container=(props)=>{
+   
+const [state, dispatch]=useReducer(Reducer,initialState)
+const [currentId,setCurrentId]=useState(null)
+useEffect(()=>{
+// axios.get("/posts").then(res=>{
+//     if(){
+   // {type:"FETCH_ALL", payload:res.data.data}
+    api.getPosts(dispatch)
+// })
+},[])
+return (
+    <MyContext.Provider value={{state,dispatch,currentId,setCurrentId}}>
+        {props.children}
+    </MyContext.Provider>
+)
+}
+export default Container
---
+++ client/src/reducers/Reducer.js
@@ -0,0 +1,19 @@
+const Reducer = (state, action) => {
+    switch (action.type) {
+        case "DELETE":
+            return {...state, posts:state.posts.filter((post)=>post._id !==action.payload)}
+        case "UPDATE":
+            return {...state,posts:state.posts.map((post)=>post._id===action.payload._id? action.payload: post)}
+        case "LIKE":    
+            return {...state,posts:state.posts.map((post)=>post._id===action.payload._id? action.payload: post)}
+        case "FETCH_ALL":
+            return {...state, posts:action.payload};
+        case "CREATE":
+            return {...state, posts:[...state.posts,action.payload]};
+        default:
+            return state;
+
+    }
+}
+
+export default Reducer
